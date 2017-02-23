4555d3ae35281b600b0d20fb8e20a41c2a8b051b
danyCorbineau <dany.corbineau@ynov.com>
2017-02-23 19:04:20 +0100

    Update Main1.java

--- hashcode/src/hashcode/Main1.java
+++ hashcode/src/hashcode/Main1.java
@@ -1,15 +1,31 @@
 package hashcode;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Main1 {
 
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
+		Scanner sc;
+		try {
+			sc = new Scanner(new File("me_at_the_zoo.in"));
+			
+			while(sc.hasNext())
+			{
+				System.out.println(sc.nextLine());
+			}
+			
+			sc.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 		
 		//sc.nextLine();
 		//sc.next();
-		sc.close();
+		
+		
 	}
 
 }
+
