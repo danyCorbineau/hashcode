121386542b28a7004540b25b3494c529ab85acb8
yolosu <pierre.chene@ynov.com>
2017-02-23 22:22:43 +0100

    Update Endpoint.java

--- hashcode/src/hashcode/Endpoint.java
+++ hashcode/src/hashcode/Endpoint.java
@@ -1,10 +1,14 @@
 package youtubeGame;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
+import java.util.Map.Entry;
+import java.util.TreeSet;
 
 public class Endpoint {
 	int ID;
@@ -13,9 +17,11 @@ public class Endpoint {
 	int latCache=0;
 	int[] tabID;
 	int nbrCacheConnected;
-	List<int[]> cacheServer=new ArrayList<int[]>();
-	Map<Integer,Integer> cacheVid=new HashMap<Integer, Integer>();
-;
+	LinkedHashMap<Integer, Integer> cacheServer=new LinkedHashMap<Integer, Integer>();
+	Map<Integer,Integer> requestedVid=new HashMap<Integer, Integer>();
+	LinkedHashMap<Integer,Integer> sortedRequestedVid=new LinkedHashMap<Integer, Integer>();
+	LinkedHashMap<Integer,Integer> sortedCacheServers=new LinkedHashMap<Integer, Integer>();
+
 
 	
 	public Endpoint(int ID, int latVidFromServer, int nbrCacheConnected){
@@ -31,10 +37,10 @@ public class Endpoint {
 		int latence;
 		String[] tabCache=lineCache.split(" ");
 		int[] tabCacheInt = new int[2];
-		//System.out.println(tabCache[0]+";"+tabCache[1]);
+		System.out.println("data"+tabCache[0]+";"+tabCache[1]);
 		tabCacheInt[0]=Integer.parseInt(tabCache[0].trim());
 		tabCacheInt[1]=Integer.parseInt(tabCache[1].trim());
-		cacheServer.add(tabCacheInt);
+		cacheServer.put(tabCacheInt[0], tabCacheInt[1]);
 
 	}
 	public int getCacheConnected(){
@@ -43,9 +49,24 @@ public class Endpoint {
 	public void sortCacheLatency(){
 		Map<Integer,Integer> sortedMap = new HashMap<Integer,Integer>();
 		int min = 0;
+		TreeSet<Integer> ts = new TreeSet<Integer>(cacheServer.values());
+		for (int value : ts){
+			for(int keyTemp : cacheServer.keySet()){
+				int valueTemp = cacheServer.get(keyTemp);
+				if( valueTemp == value){
+					System.out.println(valueTemp);
+					sortedCacheServers.put(keyTemp, valueTemp);
+					continue;
+				}
+			}
+		}
+		for(int key : sortedCacheServers.keySet()){
+			System.out.println("cacheSserver = "+key+" "+sortedCacheServers.get(key));
+		}
 		
 		
-		while(sortedMap.size()<cacheServer.size()){
+		
+		/*while(sortedMap.size()<cacheServer.size()){
 			int[] tabTemp;//recup de la premiere valeur de la liste
 			int tempMin=500;//mise de la latence max
 			for(int i=0;i<cacheServer.size();i++){//test de la valeur minimale
@@ -59,10 +80,51 @@ public class Endpoint {
 				}
 				
 			}
-			cacheServer.remove(min);
-		}
+		}*/
 	}
 	public void nbrReqIDVid(int VidID, int nbrReq){
-		//cacheVid.put(VidID, nbrReq);
+		requestedVid.put(VidID, nbrReq);
+	}
+	public void sortVidByReq(){//du plus au moins
+	
+		TreeSet<Integer> ts = new TreeSet<Integer>(requestedVid.values());
+		for (int value : ts){
+			for(int keyTemp : requestedVid.keySet()){
+				int valueTemp = requestedVid.get(keyTemp);
+				if( valueTemp == value){
+					sortedRequestedVid.put(keyTemp, valueTemp);
+					continue;
+				}
+			}
+		}
+	
+		/*int temp=0;
+		int idTemp=0;
+		int numberOfRequestTemp=0;
+		for(int i=0;i<requestedVid.size();i++){
+			for(int id : requestedVid.keySet()){
+				if(id == i){
+					numberOfRequestTemp=requestedVid.get(id);
+					if(numberOfRequestTemp > temp){
+						temp = requestedVid.get(id);
+						idTemp = id;
+						System.out.println("bonjour mm idtemp"+idTemp);
+					}
+				}
+			} 
+			for(int id : requestedVid.keySet()){
+				if(id == idTemp){
+					sortedRequestedVid.put(idTemp, numberOfRequestTemp);
+					//requestedVid.remove(idTemp);
+				}
+			}
+		}*/
+		
+		
+	for(int key : sortedRequestedVid.keySet()){
+		System.out.println(key+" "+sortedRequestedVid.get(key));
+	}
+		
+		
 	}
 }
