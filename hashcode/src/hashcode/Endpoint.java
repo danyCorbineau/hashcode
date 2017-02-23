c2067f0c136f8fb6d03c561b5a7183162e3da49c
yolosu <pierre.chene@ynov.com>
2017-02-23 20:01:01 +0100

    vpila

--- hashcode/src/hashcode/Endpoint.java
+++ hashcode/src/hashcode/Endpoint.java
@@ -1,16 +1,22 @@
 package youtubeGame;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Endpoint {
 	int NbrReqVid=0;
 	int latVidFromServer=0;
 	int latCache=0;
 	int[] tabID;
 	int nbrCacheConnected;
+	Map<Integer,Integer> cacheServer;
+
 	
 	public Endpoint(int latVidFromServer, int nbrCacheConnected){
 		this.latVidFromServer=latVidFromServer;
 		this.nbrCacheConnected=nbrCacheConnected;
 		tabID=new int[nbrCacheConnected];
+		
 	}
 	
 	public void ajouter(String lineCache){
@@ -19,8 +25,10 @@ public class Endpoint {
 		String[] tabCache=lineCache.split(" ");
 		System.out.println(tabCache[0]+";"+tabCache[1]);
 		idCache=Integer.parseInt(tabCache[0].trim());
-		latence=Integer.parseInt(tabCache[1]);
-		
-		
+		latence=Integer.parseInt(tabCache[1].trim());
 	}
+	public int getCacheConnected(){
+		return this.nbrCacheConnected;
+	}
+	public void sortCacheLatency()
 }
